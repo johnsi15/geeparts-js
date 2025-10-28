e2367ebf86ddb8adc83ad2353a2822ce439df55d
John Serrano <andrey_201015@hotmail.com>
2025-10-28 11:05:19 -0500

    Actualizar versión a 1.4.4 y eliminar clase 'new' del menú de navegación

--- index.js
+++ index.js
@@ -1812,7 +1812,7 @@ if (window.matchMedia('(min-width: 768px)').matches) {
           <li>
             <a href="/bera">Bera</a>
             <ul class="menu_level_three">
-              <li class="new">
+              <li>
                 <a class="nav-list-link" href="https://www.geeparts.co/bera/kavak/">Kavak</a>
               </li>
             </ul>
