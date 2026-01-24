8d0b966eccb8c2cf40472a6c95db5e656b280dc9
John Serrano <andrey_201015@hotmail.com>
2026-01-24 18:00:00 -0500

    Actualizar versión a 1.5.7 y agregar enlace para Spools en la barra de navegación

--- index.js
+++ index.js
@@ -1858,6 +1858,7 @@ if (window.matchMedia('(min-width: 768px)').matches) {
           <li><a href="/manubrios">Manubrios</a></li> 
           <li><a href="/sliders">Sliders</a></li>
           <li><a href="/baul">Baúl</a></li>
+          <li><a href="/spools">Spools</a></li>
           <li><a href="/caballetetrasero">Caballete trasero</a></li>      
           <li><a href="/porta-placas">Porta placas</a></li>      
           <li><a href="/corta-vientos">Corta vientos</a></li>
